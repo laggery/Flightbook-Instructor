55a4ecf3c9eaf1d4990a9c8febe3f16b867e745d
Yannick Lagger <lagger.yannick@gmail.com>
2022-04-08 23:57:55 +0200

    Responsive Student page

--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, map, Observable, Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { User } from '../../shared/domain/user';
 import { environment } from 'src/environments/environment';
