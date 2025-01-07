328f381f5ec4df99e2d707d52da57c3e7cee78bd
Yannick Lagger <lagger.yannick@gmail.com>
2025-01-07 21:32:26 +0100

    Update to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClient, HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpAuthInterceptor } from './core/interceptor/auth.interceptor';
 import { HttpErrorInterceptor } from './core/interceptor/error.interceptor';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -16,29 +16,24 @@ export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      }
-    }),
-    HttpClientModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSnackBarModule
-  ],
-  providers: [
-    DatePipe,
-    { provide: HTTP_INTERCEPTORS, useClass: HttpAuthInterceptor, multi: true},
-    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true}
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [HttpClient]
+            }
+        }),
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatSnackBarModule], providers: [
+        DatePipe,
+        { provide: HTTP_INTERCEPTORS, useClass: HttpAuthInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
