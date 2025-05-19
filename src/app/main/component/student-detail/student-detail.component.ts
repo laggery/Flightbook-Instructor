8dfc68dedbb317b56dd141622180caec3d89641d
Yannick Lagger <lagger.yannick@gmail.com>
2025-05-19 23:10:56 +0200

    Print only validated flights

--- src/app/main/component/student-detail/student-detail.component.ts
+++ src/app/main/component/student-detail/student-detail.component.ts
@@ -158,6 +158,9 @@ export class StudentDetailComponent implements OnInit, OnChanges, OnDestroy {
 
     const flights = await firstValueFrom(this.studentService.getFlightsByStudentId({ limit: 2000 }, this.student?.id!))
 
+    if (this.school?.configuration?.validateFlights) {
+      flights.entity = flights.entity!.filter((flight: Flight) => flight.validation?.state == FlightValidationState.VALIDATED);
+    }
     this.pdfExportService.generatePdf(flights.entity!, this.student?.user);
   }
 
